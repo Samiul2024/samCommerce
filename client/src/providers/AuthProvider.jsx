08fa17c309b2eb069baea9ac565f145c85e7eef0
Samiul2024 <mdsamiullahossen@gmail.com>
2026-06-07 15:12:00 +0600

    add: authprovider added

---
+++ client/src/providers/AuthProvider.jsx
@@ -0,0 +1,48 @@
+import {
+  getAuth,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+import React, {
+  createContext,
+  useEffect,
+  useState,
+} from "react";
+
+import app from "../firebase/firebase.config";
+
+export const AuthContext =
+  createContext(null);
+
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const [loading, setLoading] =
+    useState(true);
+
+  useEffect(() => {
+    const unsubscribe =
+      onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+        setLoading(false);
+      });
+
+    return unsubscribe;
+  }, []);
+
+  const authInfo = {
+    user,
+    loading,
+    auth,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
