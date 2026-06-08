a40abba9b47cf59bd0e5eca43815aa1fbd2a762c
Samiul2024 <mdsamiullahossen@gmail.com>
2026-06-08 14:45:00 +0600

    feat(auth): setup firebase authentication infrastructure

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { RouterProvider } from "react-router";
-
 import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
-
 import { Toaster } from "react-hot-toast";
 
+import AuthProvider from "./providers/AuthProvider";
 import router from "./routes/router";
 
 import "./index.css";
@@ -20,9 +19,10 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-
-      <Toaster position="top-right" />
+      <AuthProvider>
+        <RouterProvider router={router} />
+        <Toaster position="top-right" />
+      </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
